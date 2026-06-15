a7390415ad28133caf7b16a2bc5490a39fe5ab54
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-15 18:25:01 +0300

    Update StatsController.java

--- stats/stats-server/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -1,53 +1,50 @@
-package ru.practicum.stats.controller;
+package ru.practicum.ewm.stats.server.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.stats.dto.EndpointHit;
-import ru.practicum.stats.dto.ViewStats;
-import ru.practicum.stats.model.Hit;
-import ru.practicum.stats.repository.StatsRepository;
+import ru.practicum.ewm.stats.dto.EndpointHitDto;
+import ru.practicum.ewm.stats.dto.ViewStatsDto;
+import ru.practicum.ewm.stats.server.exception.BadRequestException;
+import ru.practicum.ewm.stats.server.service.StatsService;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
 @RestController
+@Slf4j
 @RequiredArgsConstructor
+@RequestMapping
 public class StatsController {
-    private final StatsRepository statsRepository;
+    private final StatsService service;
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveHit(@RequestBody EndpointHit endpointHit) {
-        log.info("Saving hit: app={}, uri={}, ip={}, timestamp={}",
-                 endpointHit.getApp(), endpointHit.getUri(),
-                 endpointHit.getIp(), endpointHit.getTimestamp());
-
-        Hit hit = Hit.builder()
-                .app(endpointHit.getApp())
-                .uri(endpointHit.getUri())
-                .ip(endpointHit.getIp())
-                .timestamp(endpointHit.getTimestamp())
-                .build();
-
-        statsRepository.save(hit);
+    public void saveHit(@RequestBody @Valid EndpointHitDto hitDto) {
+        service.saveHit(hitDto);
     }
 
     @GetMapping("/stats")
-    public List<ViewStats> getStats(
+    public List<ViewStatsDto> getStats(
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
             @RequestParam(required = false) List<String> uris,
             @RequestParam(defaultValue = "false") Boolean unique) {
 
-        log.info("Getting stats: start={}, end={}, uris={}, unique={}", start, end, uris, unique);
-
-        if (unique) {
-            return statsRepository.getUniqueStats(start, end, uris);
+        if (start.isAfter(end)) {
+            throw new BadRequestException("start must be before or equal to end");
         }
-        return statsRepository.getStats(start, end, uris);
+
+        if (uris != null) {
+            uris = uris.stream()
+                    .filter(s -> s != null && !s.isBlank())
+                    .toList();
+            if (uris.isEmpty()) uris = null;
+        }
+
+        return service.getStats(start, end, uris, unique);
     }
 }
