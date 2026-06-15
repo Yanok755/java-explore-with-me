9be44fd1dd58b924029ec94e564769b86c4d371c
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-15 23:45:49 +0300

    Create EndpointHit.java

---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/model/EndpointHit.java
@@ -0,0 +1,32 @@
+package ru.practicum.ewm.stats.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "stats")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "app", nullable = false)
+    private String app;
+
+    @Column(name = "uri", nullable = false)
+    private String uri;
+
+    @Column(name = "ip", nullable = false)
+    private String ip;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+}
