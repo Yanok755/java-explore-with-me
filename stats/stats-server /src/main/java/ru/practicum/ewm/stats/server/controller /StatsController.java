595db4d874fa8804a1d577c7e21235899c5bd21b
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-15 20:15:55 +0300

    Create StatsController.java

---
+++ stats/stats-server /src/main/java/ru/practicum/ewm/stats/server/controller /StatsController.java
@@ -0,0 +1,45 @@
+package ru.practicum.ewm.stats.server.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.stats.dto.EndpointHitDto;
+import ru.practicum.ewm.stats.dto.ViewStatsDto;
+import ru.practicum.ewm.stats.server.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping
+public class StatsController {
+    private final StatsService service;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveHit(@RequestBody @Valid EndpointHitDto hitDto) {
+        service.saveHit(hitDto);
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStatsDto> getStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") Boolean unique) {
+
+        if (uris != null) {
+            uris = uris.stream()
+                    .filter(s -> s != null && !s.isBlank())
+                    .toList();
+            if (uris.isEmpty()) uris = null;
+        }
+
+        return service.getStats(start, end, uris, unique);
+    }
+}
