c09a8b9fee3e98f4eef266222ee55dff505b6c6a
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-16 19:18:00 +0300

    Update StatsClient.java

--- stats/stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
@@ -1,18 +1,24 @@
 package ru.practicum.ewm.stats.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
+import ru.practicum.ewm.stats.dto.ViewStatsDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class StatsClient {
-    private final org.springframework.web.client.RestTemplate restTemplate;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final RestTemplate restTemplate;
 
-    @Autowired
     public StatsClient(@Value("${stats-server.url:http://localhost:9090}") String serverUrl,
                        RestTemplateBuilder builder) {
         this.restTemplate = builder
@@ -22,5 +28,20 @@ public class StatsClient {
 
     public ResponseEntity<Object> saveHit(EndpointHitDto hitDto) {
         return restTemplate.postForEntity("/hit", hitDto, Object.class);
+    }
+
+    public ResponseEntity<List<ViewStatsDto>> getStats(LocalDateTime start, LocalDateTime end,
+                                                       List<String> uris, Boolean unique) {
+        StringBuilder url = new StringBuilder("/stats?start=")
+                .append(start.format(FORMATTER))
+                .append("&end=").append(end.format(FORMATTER))
+                .append("&unique=").append(unique);
+        if (uris != null && !uris.isEmpty()) {
+            for (String uri : uris) {
+                url.append("&uris=").append(uri);
+            }
+        }
+        ViewStatsDto[] response = restTemplate.getForObject(url.toString(), ViewStatsDto[].class);
+        return ResponseEntity.ok(response != null ? Arrays.asList(response) : List.of());
     }
 }
