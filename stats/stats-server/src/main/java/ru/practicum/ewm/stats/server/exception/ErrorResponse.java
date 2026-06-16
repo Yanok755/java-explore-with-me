85f77f126f68712e580f1bea06ef8576a21eb127
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-16 19:20:31 +0300

    Create ErrorResponse.java

---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package ru.practicum.ewm.stats.server.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private String reason;
+    private String status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
