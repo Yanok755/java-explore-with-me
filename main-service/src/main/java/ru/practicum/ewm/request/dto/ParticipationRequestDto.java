d7279b4b82282d26e73be08787148eab67e05599
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-06-16 19:09:10 +0300

    Create ParticipationRequestDto.java

---
+++ main-service/src/main/java/ru/practicum/ewm/request/dto/ParticipationRequestDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.ewm.request.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipationRequestDto {
+    private Long id;
+    private Long event;
+    private Long requester;
+    private String status;
+    private String created;
+}
